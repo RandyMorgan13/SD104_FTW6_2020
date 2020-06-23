a5f7b4dd8b9ffba124e7ebf31e1a0264aba8717e
Randy Morgan <morgan13231323@yahoo.com>
2020-06-23 11:41:11 -0500

    Re-Submit

---
+++ Lab4_5/Lab4_5/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace _4_5Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Create a list to hold the scores.
+            List<int> scores = new List<int>();
+            scores.Add(1200);
+            scores.Add(4400);
+            scores.Add(3700);
+            scores.Add(8900);
+            scores.Add(7200);
+
+
+            //2.Sort the list low-high
+            scores.Sort();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            //3.Sort the list high-low
+            scores.Reverse();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            //4.Add a new score
+            scores.Add(2400);
+
+            //5. Remove Score
+            scores.Remove(4400);
+
+        }
+    }
+}
