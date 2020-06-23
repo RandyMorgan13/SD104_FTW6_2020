8e1c274ee929287dbbb32d0f3c5ac6ef22b82868
Randy Morgan <morgan13231323@yahoo.com>
2020-06-23 11:38:05 -0500

    Re-Submit

---
+++ 3_5Lab/3_5Lab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson3SpamChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "Expletive";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("You can't say that!");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("Yeah, you can say that.");
+            }
+        }
+    }
+}
